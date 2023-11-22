7699351bdb8fc3055777a18e1ffa8e9f28d3b008
boyo <zhijunc664@gmail.com>
2023-11-22 17:43:00 +0800

    20231122 update

--- demo-operator/src/DemoOperator.java
+++ demo-operator/src/DemoOperator.java
@@ -90,6 +90,96 @@ public class DemoOperator {
         }else{
             System.out.println("Example 6 is not pass");
         }
+        // boolean -> && ||
+        boolean b1 = true && false;
+        System.out.println("b1 = "+b1);
 
+        boolean b2 = false;
+        boolean b3 = true;
+
+        boolean result1 = b2 || b3;
+
+        System.out.println("Result1 = " + result1);
+
+        boolean result2 = b2 || b3 && b1;
+
+        System.out.println("Result2 = " + result2);
+
+        if (b1 == false) {
+            System.out.println("Example 7: (b1 == false) pass");//print
+        } 
+
+         if (b1 == true) {
+            System.out.println("(b1 == true) is pass");//not print
+        }
+
+         if (!b1) {
+            System.out.println("(!b1) is pass");//print
+        }
+        
+        if (b1) {
+            System.out.println("Example 8: (b1) is pass");//not print
+        }
+
+        //1
+        int g = 0;
+        if (b1 == false) {
+            g = 10;
+            System.out.println("g = " + g);
+        }
+
+        //2
+        int k = b1 == false ? 10 : 0;
+        System.out.println("k = " + k);//1 and 2 is same
+        //if b1 is false, assign 10 to k,otherwise 0
+        
+        int a1 = 10;
+        int a2 = 4;
+        //1
+        int f = 20;
+        int min;
+        if (f < 10) {
+             min = a1;
+        }else{
+            min = a2;
+        }
+        System.out.println("min = " + min);
+        //2
+        int min1 = f < 10 ? a1:a2;
+        System.out.println("min1 = " + min1);
+
+        int number = 13;
+        System.out.println("-------------");
+        //if
+        //judge a number is odd number
+        Boolean isOdd = true;
+        if (number % 2 == 0) {
+             isOdd = false;
+        }
+        System.out.println("Odd Number:" + isOdd);
+         //judge a number is prime number
+         Boolean isPrime = true;
+        for(int i = 2;i < number;i++){
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+        System.out.println("Prime Numbers:" + isPrime);
+
+        int itemTotal = 1002;
+        double totalAmount = 0.0d;
+        double freeRate = 0.03;
+        //if itemTotal > 3000, no transportation fee,otherwise +3% transportation
+        /*if (itemTotal <= 3000) {
+            totalAmount = itemTotal * (1 + freeRate);
+        }else{
+            totalAmount = itemTotal;
+        }*/
+        double transportation =0.0d;
+        if(itemTotal <= 3000){
+            transportation = itemTotal * freeRate;
+        }
+        totalAmount = itemTotal + transportation;
+        System.out.println("Total Amount = " + totalAmount);
     }
 }
